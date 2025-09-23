b23e25222d3fb95deec486b7f048734065054e8d
Nishu <nishanthravichandran12@gmail.com>
2025-09-23 15:49:31 +0530

    Define BNodes and BNode Lookups

---
+++ bptree.go
@@ -0,0 +1,106 @@
+package nishudb
+
+import (
+	"bytes"
+	"encoding/binary"
+)
+
+type BNode []byte // no serialization required, can be dumped to disk
+
+const HEADER = 4
+const BTREE_PAGE_SIZE = 4096
+const BTREE_MAX_KEY_SIZE = 1000
+const BTREE_MAX_VAL_SIZE = 3000
+
+const (
+	BNODE_NODE = 1 // internal nodes without values
+	BNODE_LEAF = 2 // leaf nodes with values
+)
+
+type BTree struct {
+	// pointer (a nonzero page number)
+	root uint64
+	// callbacks for managing on-disk pages
+	get func(uint64) []byte // dereference a pointer
+	new func([]byte) uint64 // allocate a new page
+	del func(uint64)        // deallocate a page
+}
+
+func assert(cond bool) {
+	if !cond {
+		panic("Error!")
+	}
+}
+
+func (node BNode) btype() uint16 {
+	return binary.LittleEndian.Uint16(node[0:2])
+}
+func (node BNode) nkeys() uint16 {
+	return binary.LittleEndian.Uint16(node[2:4])
+}
+func (node BNode) setHeader(btype uint16, nkeys uint16) {
+	binary.LittleEndian.PutUint16(node[0:2], btype)
+	binary.LittleEndian.PutUint16(node[2:4], nkeys)
+}
+
+func (node BNode) getPtr(idx uint16) uint64 {
+	assert(idx < node.nkeys())
+	pos := HEADER + 8*idx
+	return binary.LittleEndian.Uint64(node[pos:])
+}
+func (node BNode) setPtr(idx uint16, val uint64)
+
+func offsetPos(node BNode, idx uint16) uint16 {
+	assert(1 <= idx && idx <= node.nkeys())
+	return HEADER + 8*node.nkeys() + 2*(idx-1)
+}
+
+func (node BNode) getOffset(idx uint16) uint16 {
+	if idx == 0 {
+		return 0
+	}
+	return binary.LittleEndian.Uint16(node[offsetPos(node, idx):])
+}
+
+func (node BNode) setOffset(idx uint16, offset uint16)
+
+func (node BNode) kvPos(idx uint16) uint16 {
+	assert(idx <= node.nkeys())
+	return HEADER + 8*node.nkeys() + 2*node.nkeys() + node.getOffset(idx)
+}
+
+func (node BNode) getKey(idx uint16) []byte {
+	assert(idx < node.nkeys())
+	pos := node.kvPos(idx)
+	klen := binary.LittleEndian.Uint16(node[pos:])
+	return node[pos+4:][:klen] //why not slice it at the same time?
+}
+func (node BNode) getVal(idx uint16) []byte
+
+func (node BNode) nbytes() uint16 {
+	return node.kvPos(node.nkeys())
+}
+
+// TODO: binary search
+func nodeLookupLE(node BNode, key []byte) uint16 {
+	nkeys := node.nkeys()
+	found := uint16(0)
+	// the first key is a copy from the parent node,
+	// thus it's always less than or equal to the key.
+	for i := uint16(1); i < nkeys; i++ {
+		cmp := bytes.Compare(node.getKey(i), key)
+		if cmp <= 0 {
+			found = i
+		}
+		if cmp >= 0 {
+			break
+		}
+	}
+	return found
+}
+
+func init() {
+	node1max := HEADER + 8 + 2 + 4 + BTREE_MAX_KEY_SIZE + BTREE_MAX_VAL_SIZE // 8 is subtree pointer, 2 is offset, 4 is keylen and vallen(2 each)
+	assert(node1max <= BTREE_PAGE_SIZE)
+
+}
